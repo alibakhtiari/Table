baef90bc9923f50627afdfd4912d92a9cea438b2
alibakhtiari <ali.bakhtiarii@gmail.com>
2021-10-29 01:36:15 +0330

    start using javascript fetch
    fetching coin prices using coincap.io api

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,17 @@
 import '../css/style.css'
+
+var requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+    };
+    
+    var promise = Promise.race([
+    fetch('https://api.coincap.io/v2/assets?limit=10', requestOptions)
+    .then(response => response.text()),
+    new Promise((resolve, reject) =>
+    setTimeout(() => reject(new Error('Timeout')), 3200)
+    )
+    ]);
+    
+    promise.then(result => console.log(result)),
+    promise.catch(error => console.log(error));
