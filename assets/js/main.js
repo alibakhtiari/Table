986f552047b4d8abd4bfe8c1f69a9b0aedd7201f
alibakhtiari <ali.bakhtiarii@gmail.com>
2021-10-29 13:38:39 +0330

    visual updates to cards ui

--- assets/js/main.js
+++ assets/js/main.js
@@ -59,19 +59,26 @@ fetch(myRequest)
     .then(data => {
       if (data.data.length > 0) {
         var coins = "",
-        color;
+        color,bg_color;
         data.data.forEach((item) => {
           if(parseFloat(item.changePercent24Hr).toFixed(2) > 0){
-            color = 'bg-green-300'
+            bg_color = 'bg-green-400';
           }
           else{
-            color = 'bg-red-300'
+            bg_color = 'bg-red-400'
           }
-          coins += '<div class="w-full flex flex-col justify-center items-center rounded shadow-lg bg-orange-50">';
-          coins += '<img class="coin-img w-16 h-16" src="coins/' + item.symbol + '.svg" alt="' + item.id + '">';
-          coins += '<h2 class="coin-header capitalize ltr text-center font-bold">'+ item.rank + '. '+ item.id + '<br><span class="text-xs"> (' + item.symbol + ')</span></h2>';
-          coins +=  '<div class="flex justify-center align-middle text-center items-center"><div><h5 class="coin-price-usd">$' + ThousandSep(parseFloat(item.priceUsd).toFixed(2)) + '</h5><h6 class="rtl">' + ThousandSep(Math.floor(item.priceUsd * dollarp / 10)) + ' تومان </h6></div><div class="pl-5"><h3 id="coin_percentage" class="px-2 py-5 text-sm rounded-full font-bold '+ color +' ">' + parseFloat(item.changePercent24Hr).toFixed(2) + '%</h3></div></div>';
-          coins +='<div class="rtl text-xs inline-flex"><h6 class="pl-5 font-semibold">حجم بازار:</h6><span class="">'+ numberFormatter(item.marketCapUsd) +'</span></div></div>'
+          if(parseFloat(item.vwap24Hr*item.changePercent24Hr/100).toFixed(2) > 0){
+            color = 'text-green-500';
+          }
+          else{
+            color = 'text-red-500'
+          }
+          coins += '<div class="w-full flex flex-col justify-center items-center rounded shadow-lg bg-orange-100">';
+          coins += '<img class="coin-img w-14 h-14 mt-5" src="coins/' + item.symbol + '.svg" alt="' + item.id + '">';
+          coins += '<h2 class="coin-header capitalize ltr text-center font-bold p-2">'+ item.rank + '. '+ item.id + '<br><span class="text-xs"> (' + item.symbol + ')</span></h2>';
+          coins += '<div class="flex justify-center align-middle text-center items-center p-2"><div><h5 class="coin-price-usd">$' + ThousandSep(parseFloat(item.priceUsd).toFixed(2)) + '</h5><h6 class="rtl">' + ThousandSep(Math.floor(item.priceUsd * dollarp / 10)) + ' تومان </h6></div><div class="pl-5"><h3 id="coin_percentage" class="flex align-middle justify-center items-center p-2 text-sm rounded-full font-bold '+ bg_color +' text-white w-14 h-14">' + parseFloat(item.changePercent24Hr).toFixed(2) + '%</h3></div></div>';
+          coins += '<div class="text-right m-5 p-3 bg-white border border-orange-300 rtl text-xs"><div class="inline-flex py-2 justify-between w-full"><h6 class="pl-5 font-semibold">حجم بازار:</h6><span class="font-bold">'+ numberFormatter(item.marketCapUsd) +'</span></div>';
+          coins += '<div class="inline-flex pb-2 w-full justify-between"><h6 class="pl-5 font-semibold">تغییرات 24 ساعته:</h6><span class="'+ color +' ltr font-bold">'+ ThousandSep(parseFloat(item.vwap24Hr*item.changePercent24Hr/100).toFixed(2)) +'</span><span class="'+ color +' pr-1 font-bold"> دلار</span></div></div></div>';
           
         });
         document.getElementById('coins_outer').innerHTML = coins;
@@ -125,7 +132,7 @@ fetch("/simple-data/2.json")
       default:
       cryptofarsi = item;
     }
-      market_caps += '<tr class="odd:bg-orange-50"><td class="w-2/3 text-right py-3 px-4 border border-orange-200"><img class="w-6 h-6 inline-block" src="coins/' + item + '.svg" alt="' + item + '"> '+ cryptofarsi + ' </td><td class="w-1/3 text-center py-3 px-4 border border-orange-200">' + parseFloat(percentage[item]).toFixed(2) + ' %</td></tr>';
+      market_caps += '<tr class="odd:bg-orange-100"><td class="w-2/3 text-right py-3 px-4 border border-orange-200"><img class="w-6 h-6 inline-block" src="coins/' + item + '.svg" alt="' + item + '"> '+ cryptofarsi + ' </td><td class="w-1/3 text-center py-3 px-4 border border-orange-200">' + parseFloat(percentage[item]).toFixed(2) + ' %</td></tr>';
 
   }
   document.getElementById('market_caps').innerHTML = market_caps;
