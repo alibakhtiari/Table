12b99947f06ee25d322e81f48b1bcc1de203cdfd
alibakhtiari <ali.bakhtiarii@gmail.com>
2022-03-02 02:28:32 +0330

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,16 +15,15 @@ function status(response) {
     return Promise.reject(new Error(response.statusText))
   }
 }
-
 function json(response) {
   return response.json()
 }
 var dollarp;
-fetch('https://api.accessban.com/v1/market/widget-tmp?keys=137203' , requestOptions)
+fetch('https://arzdigital.com/wp-json/app/v1/coins/9' , requestOptions)
   .then(status)
   .then(json)
   .then(function (data) {
-    return dollarp = data.response.indicators[0].p;
+    return dollarp = data.price;
   })
   .then(function get_cc_p() {
       fetch("https://3rdparty-apis.coinmarketcap.com/v1/cryptocurrency/top" , requestOptions)
@@ -47,7 +46,7 @@ fetch('https://api.accessban.com/v1/market/widget-tmp?keys=137203' , requestOpti
               color = 'text-red-500'
             }
           coins += '<div class="w-full flex flex-col justify-center items-center shadow-lg ' + bg_shade + ' rounded-lg">';
-          coins += '<div class="inline-flex items-center my-3"><img class="coin-img w-12 h-12" src="' + item.logo + '" alt="' + item.name + '">';
+          coins += '<div class="inline-flex items-center my-2"><img class="coin-img w-12 h-12" src="' + item.logo + '" alt="' + item.name + '">';
           coins += '<h2 class="coin-header capitalize ltr text-center font-bold ml-2 ' + item.symbol + '">' + item.cmc_rank + '. ' + item.name + '<br><span class="text-xs"> (' + item.symbol + ')</span></h2></div>';
           coins += '<div class="flex justify-center align-middle text-center items-center p-2"><div><h5 class="coin-price-usd">$' + ThousandSep(parseFloat(item.price).toFixed(2)) + '</h5><h6 class="rtl">' + ThousandSep(Math.floor(item.price * dollarp / 10)) + ' تومان </h6></div><div class="pl-5"><h3 id="coin_percentage" class="flex align-middle justify-center items-center p-2 text-sm rounded-full font-bold ' + bg_color + ' text-white w-14 h-14">' + parseFloat(item.percent_change_24h).toFixed(2) + '%</h3></div></div></div>';
         }});
