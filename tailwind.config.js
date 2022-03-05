df8ca78435a543ef94450b0788995f957690b497
alibakhtiari <ali.bakhtiarii@gmail.com>
2022-03-05 20:23:33 +0330

    fix typo

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const colors = require('tailwindcss/colors')
 module.exports = {
     corePlugins: {
         preflight: false,
-    }
+    },
     purge: {
         enabled: true,
         content:[
